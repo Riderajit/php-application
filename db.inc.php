1a912dcecb08aa3a8cce996aa7b932f53362765d
Ajith Chettiyar <112484343+Riderajit@users.noreply.github.com>
2024-09-25 14:15:46 +0530

    Update db.inc.php

--- db.inc.php
+++ db.inc.php
@@ -1,7 +1,7 @@
 <?php
 
-define('DB_SERVER', 'first-rds-php.cplnmrmbfwyc.us-east-1.rds.amazonaws.com');
+define('DB_SERVER', 'web-appliaction.cplnmrmbfwyc.us-east-1.rds.amazonaws.com');
 define('DB_USERNAME', 'admin');
 define('DB_PASSWORD', 'Ajith3010');
-define('DB_DATABASE', 'first-rds-php');
+define('DB_DATABASE', 'web-appliaction');
 ?>
