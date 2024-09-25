17dfda83e715f8e00daaab3b6c239f8066825e03
Ajith Chettiyar <112484343+Riderajit@users.noreply.github.com>
2024-09-25 14:53:05 +0530

    Update db.inc.php

--- db.inc.php
+++ db.inc.php
@@ -3,5 +3,5 @@
 define('DB_SERVER', 'web-appliaction.cplnmrmbfwyc.us-east-1.rds.amazonaws.com');
 define('DB_USERNAME', 'admin');
 define('DB_PASSWORD', 'Ajith3010');
-define('DB_DATABASE', 'web-appliaction');
+define('DB_DATABASE', 'your_database_name');
 ?>
