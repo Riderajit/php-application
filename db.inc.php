837b32859c925678ced058711fc2f214cf933312
root <root@ip-172-31-88-98.ec2.internal>
2024-09-25 09:59:26 +0000

    Initial commit with PHP application and Docker setup

---
+++ db.inc.php
@@ -0,0 +1,7 @@
+<?php
+
+define('DB_SERVER', 'web-appliaction.cplnmrmbfwyc.us-east-1.rds.amazonaws.com');
+define('DB_USERNAME', 'admin');
+define('DB_PASSWORD', 'Ajith3010');
+define('DB_DATABASE', 'your_database_name');
+?>
